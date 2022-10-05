f6b6cb17fff4a0496f7cc1fb98be1355f7f2d124
TheLazyBit <daniel.kogan@gmx.net>
2022-10-05 20:16:17 +0200

    setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,49 @@
-{
-  "name": "dsa-animation-stuff",
-    "version": "0.1.0",
-    "private": true,
-    "dependencies": {
-  "@testing-library/jest-dom": "^5.16.5",
-      "@testing-library/react": "^13.4.0",
-      "@testing-library/user-event": "^13.5.0",
-      "@types/jest": "^27.5.2",
-      "@types/node": "^16.11.59",
-      "@types/react": "^18.0.20",
-      "@types/react-dom": "^18.0.6",
-      "eslint-config-airbnb": "^19.0.4",
-      "eslint-config-airbnb-typescript": "^17.0.0",
-      "react": "^18.2.0",
-      "react-dom": "^18.2.0",
-      "react-router-dom": "^6.4.0",
-      "react-scripts": "5.0.1",
-      "sass": "^1.54.9",
-      "web-vitals": "^2.1.4"
-},
-  "scripts": {
-  "start": "react-scripts start",
-      "build": "react-scripts build",
-      "test": "react-scripts test",
-      "eject": "react-scripts eject"
-},
-  "eslintConfig": {
-  "extends": [
-    "react-app",
-    "react-app/jest"
-  ]
-},
-  "browserslist": {
-  "production": [
-    ">0.2%",
-    "not dead",
-    "not op_mini all"
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
   ],
-      "development": [
-    "last 1 chrome version",
-    "last 1 firefox version",
-    "last 1 safari version"
-  ]
-},
-  "devDependencies": {
-  "@typescript-eslint/eslint-plugin": "^5.37.0",
-      "@typescript-eslint/parser": "^5.37.0",
-      "eslint": "^8.22.0",
-      "eslint-config-standard-with-typescript": "^23.0.0",
-      "eslint-plugin-import": "^2.26.0",
-      "eslint-plugin-n": "^15.2.5",
-      "eslint-plugin-promise": "^6.0.1",
-      "eslint-plugin-react": "^7.31.8",
-      "eslint-plugin-react-hooks": "^4.6.0",
-      "typescript": "^4.8.3"
-}
-}
+  overrides: [
+    {
+      extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'airbnb-typescript',
+      ],
+      plugins: [
+        'react-hooks',
+      ],
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      rules: {
+        'react-hooks/rules-of-hooks': 'error',
+        '@typescript-eslint/no-loop-func': 'off',
+        'import/prefer-default-export': 'off',
+        'react/require-default-props': 'off',
+        'jsx-a11y/label-has-associated-control': 'off',
+        'react/no-unused-prop-types': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        'react/no-array-index-key': 'warn',
+        'max-len': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        '@typescript-eslint/no-redeclare': 'off',
+        'import/no-named-as-default': 'off',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
