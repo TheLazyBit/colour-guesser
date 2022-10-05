e6a98a186c290aec229da7e2f61a63aa6d1054b2
TheLazyBit <daniel.kogan@gmx.net>
2022-10-05 20:51:33 +0200

    finish

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Colour Guesesr
+Colour guessing challenge by web dev junkie.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+
+const colourRange = 256 ** 3;
+function getRandomColour() {
+  const colour = Math.floor(Math.random() * colourRange);
+  return `#${colour.toString(16)}`;
+}
 
 function App() {
+  const [colours, setColours] = useState(new Array(3).fill('').map(() => getRandomColour()));
+  const [correctColour, setCorrectColour] = useState(colours[Math.floor(Math.random() * 3)]);
+  const [guessedCorrect, setGuessedCorrect] = useState<boolean | null>(null);
+
+  function reset() {
+    const newColours = new Array(3).fill('').map(() => getRandomColour());
+    setColours(newColours);
+    setCorrectColour(newColours[Math.floor(Math.random() * 3)]);
+  }
+
+  function guess(colour: string) {
+    if (colour === correctColour) {
+      setGuessedCorrect(true);
+      reset();
+    } else {
+      setGuessedCorrect(false);
+    }
+  }
+
+  let guessStateDisplay = null;
+  if (guessedCorrect !== null) {
+    if (guessedCorrect) {
+      guessStateDisplay = <div style={{ color: 'green' }}>You are Correct!</div>;
+    } else {
+      guessStateDisplay = <div style={{ color: 'red' }}>You are Colourblind!</div>;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="colour-panel" style={{ background: correctColour }} />
+      {
+        colours.map((colour) => (
+          <button key={colour} type="button" onClick={() => { guess(colour); }}>
+            {colour}
+          </button>
+        ))
+      }
+      {
+        guessStateDisplay
+      }
     </div>
   );
 }
